fc8031cf3c1db3357d5ca619ca7eb7dc35a69a93
Pedro Alves <pedrohvs.alves@gmail.com>
2024-04-20 13:24:02 +0100

    Final

--- src/main/java/com/portifolio/tasks/repositories/TasksRepository.java
+++ src/main/java/com/portifolio/tasks/repositories/TasksRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.portifolio.tasks.entities.Tasks;
 
 public interface TasksRepository extends JpaRepository<Tasks, Long> {
-	
 }
